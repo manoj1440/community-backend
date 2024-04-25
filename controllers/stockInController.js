a8a0e32f8a83ab7ed69b677811aa2ee6a1dec6b7
manoj1440 <malviya.manoj1997@gmail.com>
2024-04-25 22:05:14 +0530

    Added completed stock in api stock in

--- controllers/stockInController.js
+++ controllers/stockInController.js
@@ -13,7 +13,7 @@ const createStockIn = async (req, res, next) => {
 
 const getAllStockIns = async (req, res, next) => {
     try {
-        const stockIns = await StockIn.find({ quantity: { $ne: 0 } }).populate('commodityId warehouseId');
+        const stockIns = await StockIn.find().populate('commodityId warehouseId');
         res.json({ status: true, message: 'StockIns fetched successfully', data: stockIns });
     } catch (error) {
         res.status(500).json({ status: false, message: 'Failed to fetch StockIns', error: error.message });
